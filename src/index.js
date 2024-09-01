4baccab554547aca64c2e87bd7e1023b7dc1476d
wenbo.bob.wu <wenbo.bob.wu@icloud.com>
2024-09-01 22:10:48 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-fetch('config/articles.json')
-  .then((response) => response.json())
-  .then((articles) => {
-    articles.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime))
-    articles.forEach((article) => {
-      const cardTemplate = document.querySelector('#card')
-      const aEls = cardTemplate.content.querySelectorAll('a')
-      for (const a of aEls) {
-        a.href = article.href
-      }
+main()
 
-      const img = cardTemplate.content.querySelector('img')
-      img.src = article.img
+function main() {
+  fetch('config/articles.json')
+    .then((response) => response.json())
+    .then((articles) => {
+      articles.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime))
+      articles.forEach((article) => {
+        const cardTemplate = document.querySelector('#card')
+        const aEls = cardTemplate.content.querySelectorAll('a')
+        for (const a of aEls) {
+          a.href = article.href
+        }
 
-      const h3 = cardTemplate.content.querySelector('h3')
-      h3.textContent = article.title
+        const img = cardTemplate.content.querySelector('img')
+        img.src = article.img
 
-      const timeEl = cardTemplate.content.querySelector('.bottom-line__time')
-      timeEl.textContent = article.dateTime
+        const h3 = cardTemplate.content.querySelector('h3')
+        h3.textContent = article.title
 
-      const listEl = document.querySelector('.article-list')
-      const clone = document.importNode(cardTemplate.content, true)
-      listEl.appendChild(clone)
+        const timeEl = cardTemplate.content.querySelector('.bottom-line__time')
+        timeEl.textContent = article.dateTime
+
+        const listEl = document.querySelector('.article-list')
+        const clone = document.importNode(cardTemplate.content, true)
+        listEl.appendChild(clone)
+      })
     })
-  })
-  .catch((error) => console.error(error))
+    .catch((error) => console.error(error))
+}
